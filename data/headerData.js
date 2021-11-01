7c03e427492ec7019a89698a9aab9251cbaba6fb
Saad Siddiqui <saadsiddiqui927@gmail.com>
2021-11-02 02:21:38 +0530

    Header elements data

---
+++ data/headerData.js
@@ -0,0 +1,28 @@
+import HomeSharpIcon from "@mui/icons-material/HomeSharp";
+import StickyNote2SharpIcon from "@mui/icons-material/StickyNote2Sharp";
+import StorefrontSharpIcon from "@mui/icons-material/StorefrontSharp";
+import NotificationsActiveRoundedIcon from "@mui/icons-material/NotificationsActiveRounded";
+import ForumRoundedIcon from "@mui/icons-material/ForumRounded";
+
+export const data = [
+  {
+    id: 1,
+    title: "",
+    Icon: HomeSharpIcon,
+  },
+  {
+    id: 2,
+    title: "news",
+    Icon: StickyNote2SharpIcon,
+  },
+  {
+    id: 3,
+    title: "market",
+    Icon: StorefrontSharpIcon,
+  },
+  {
+    id: 4,
+    title: "messages",
+    Icon: ForumRoundedIcon,
+  },
+];
