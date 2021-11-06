8cf8252587bc1296f17da1ead9fdd0c106117bee
Saad Siddiqui <saadsiddiqui927@gmail.com>
2021-11-06 19:56:54 +0530

    Add fake users

---
+++ components/Sidebar/Sidebar.js
@@ -0,0 +1,31 @@
+import faker from "faker";
+import { useState, useEffect } from "react";
+import SidebarOptions from "../SidebarOptions/SidebarOptions";
+
+const Sidebar = () => {
+  const [friends, setFriends] = useState([]);
+
+  // generating random data from faker api
+  useEffect(() => {
+    const suggestions = [...Array(5)].map((_, i) => ({
+      ...faker.helpers.contextualCard(),
+      id: i,
+    }));
+    setFriends(suggestions);
+  }, []);
+
+  return (
+    <div className="hidden flex-1 lg:inline bg-white p-2 rounded shadow-lg ml-5 mr-6 mb-auto">
+      <p className="mx-5 font-bold font-mono">Friends</p>
+      {friends.map((profile) => (
+        <SidebarOptions
+          key={profile.id}
+          name={profile.username}
+          profilePic={profile.avatar}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Sidebar;
