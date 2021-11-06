ef64d08b7f05538c80946c5c013aa96cab4ebe42
Saad Siddiqui <saadsiddiqui927@gmail.com>
2021-11-06 12:15:18 +0530

    Add new styles

--- components/Feed/Feed.js
+++ components/Feed/Feed.js
@@ -3,9 +3,11 @@ import Posts from "../Posts/Posts";
 
 const Feed = () => {
   return (
-    <div className="w-full flex-col lg:w-1/2 flex-col justify-center">
+    <div className="w-full flex-col lg:w-1/2 flex-col justify-center  ">
       <UploadContainer />
-      <Posts />
+      <div className="posts__container ">
+        <Posts />
+      </div>
     </div>
   );
 };
