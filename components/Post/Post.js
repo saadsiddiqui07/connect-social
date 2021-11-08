59c011b349c1e798a872e5c63c9557ebee209931
Saad Siddiqui <saadsiddiqui927@gmail.com>
2021-11-08 19:37:05 +0530

    Edit component

--- components/Post/Post.js
+++ components/Post/Post.js
@@ -1,9 +1,47 @@
-const Post = () => {
-  return (
-    <div className="w-full self-center bg-blue-500">
-      <h1>Post component</h1>
-    </div>
-  );
-};
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 
-export default Post;
+export default function Post({
+  username,
+  caption,
+  id,
+  profileImg,
+  timestamp,
+  image,
+}) {
+  return (
+    <Card className="m-5 w-4/5 ml-auto mr-auto">
+      <CardHeader
+        avatar={<Avatar src={profileImg} sx={{ bgcolor: red[500] }} />}
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title={username}
+        subheader="September 14, 2016"
+      />
+      <CardMedia
+        className="object-contain h-60"
+        component="img"
+        height="194"
+        image={image}
+        alt="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          This impressive paella is a perfect party dish and a fun meal to cook
+          together with your guests. Add 1 cup of frozen peas along with the
+          mussels, if you like.
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
