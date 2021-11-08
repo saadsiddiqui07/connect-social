db9640bbcd4ec22fb2bb689d11e63c1dad68cf93
Saad Siddiqui <saadsiddiqui927@gmail.com>
2021-11-08 19:38:13 +0530

    Fetch posts data from firestore

--- components/Posts/Posts.js
+++ components/Posts/Posts.js
@@ -1,12 +1,35 @@
+import { useState, useEffect } from "react";
 import Post from "../Post/Post";
+import { onSnapshot, query, collection, orderBy } from "firebase/firestore";
+import { db } from "../../firebase/firebase";
 
 const Posts = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    // fetching posts from the database
+    () =>
+      onSnapshot(
+        query(collection(db, "posts"), orderBy("timestamp", "desc")),
+        (snapshot) => {
+          setPosts(snapshot.docs);
+        }
+      );
+  }, [db]);
+  console.log(posts);
   return (
-    <div className="bg-red-200 w-full flex-col justify-center items-center">
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+    <div className=" w-full flex-col justify items-center mt-8">
+      {posts.map((post) => (
+        <Post
+          id={post.id}
+          key={post.id}
+          username={post.data().username}
+          image={post.data().image}
+          caption={post.data().caption}
+          profileImg={post.data().profileImg}
+          timestamp={post.data().timestamp}
+        />
+      ))}
     </div>
   );
 };
