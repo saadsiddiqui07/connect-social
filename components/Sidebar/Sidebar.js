30f18ab5a437f0fc116fd08c21bd62053ea3feab
Saad Siddiqui <saadsiddiqui927@gmail.com>
2021-12-30 02:01:20 +0530

    Add functionality to store chatRooms in firestore

--- components/Sidebar/Sidebar.js
+++ components/Sidebar/Sidebar.js
@@ -1,27 +1,31 @@
-import faker from "faker";
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import SidebarOptions from "../SidebarOptions/SidebarOptions";
+import { onSnapshot, query, collection, orderBy } from "firebase/firestore";
+import { db } from "../../firebase/firebase";
 
 const Sidebar = () => {
   const [friends, setFriends] = useState([]);
 
-  // generating random data from faker api
   useEffect(() => {
-    const suggestions = [...Array(5)].map((_, i) => ({
-      ...faker.helpers.contextualCard(),
-      id: i,
-    }));
-    setFriends(suggestions);
+    const unsubscribe = onSnapshot(
+      query(collection(db, "users")),
+      (snapshot) => {
+        setFriends(snapshot.docs);
+      }
+    );
+    return () => {
+      unsubscribe();
+    };
   }, []);
 
   return (
     <div className="hidden flex-1 lg:inline bg-white p-2 rounded shadow-lg ml-5 mr-6 mb-auto">
-      <p className="mx-5 font-bold font-mono">Friends</p>
+      <p className="mx-5 font-bold font-mono">Suggested</p>
       {friends.map((profile) => (
         <SidebarOptions
           key={profile.id}
-          name={profile.username}
-          profilePic={profile.avatar}
+          username={profile.data().username}
+          profilePic={profile.data().profilePic}
         />
       ))}
     </div>
