dd31de93c3914daf49fa7b9d04afce7cc4a90d7c
Saad Siddiqui <saadsiddiqui927@gmail.com>
2021-12-30 02:08:45 +0530

    Fix stylings of the component

--- pages/news/index.js
+++ pages/news/index.js
@@ -16,7 +16,7 @@ const News = ({ results }) => {
       <div className="sm:hidden">
         <MobileHeader />
       </div>
-      <div className="px-5 bg-gray-100 center-center sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
+      <div className="px-5 pt-5 bg-gray-100 center-center sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
         {results.map((result) => (
           <NewsCard
             id={result.id}
