4815449d0b6b6cc4004cd9ccfbf419c03001a292
Saad Siddiqui <saadsiddiqui927@gmail.com>
2021-12-30 01:52:40 +0530

    Add logout functionality and new styles

--- components/FloatingButton/UploadButton.js
+++ components/FloatingButton/UploadButton.js
@@ -1,10 +1,5 @@
 import { useState, useRef } from "react";
-import AddIcon from "@mui/icons-material/Add";
-import { Avatar, Fab, Box, Modal, Button } from "@mui/material";
-import ImageSearchIcon from "@mui/icons-material/ImageSearch";
-import VideoCameraBackIcon from "@mui/icons-material/VideoCameraBack";
-import CreateIcon from "@mui/icons-material/Create";
-import { useStateValue } from "../../context-api/StateProvider";
+import {useRouter} from "next/router"
 import {
   addDoc,
   collection,
@@ -12,8 +7,25 @@ import {
   doc,
   updateDoc,
 } from "firebase/firestore";
-import { db, storage } from "../../firebase/firebase";
+import {signOut} from "firebase/auth"
+import { db, storage, auth } from "../../firebase/firebase";
 import { ref, uploadString, getDownloadURL } from "firebase/storage";
+import {
+  Avatar,
+  Box,
+  Modal,
+  Button,
+  SpeedDial,
+  SpeedDialAction,
+} from "@mui/material";
+import ImageSearchIcon from "@mui/icons-material/ImageSearch";
+import VideoCameraBackIcon from "@mui/icons-material/VideoCameraBack";
+import CreateIcon from "@mui/icons-material/Create";
+import FlakyIcon from "@mui/icons-material/Flaky";
+import AddIcon from "@mui/icons-material/Add";
+import { useStateValue } from "../../context-api/StateProvider";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+
 
 const style = {
   position: "absolute",
@@ -28,21 +40,19 @@ const style = {
 };
 
 const UploadButton = () => {
-  const [{ user }] = useStateValue();
+  const [{ user }, dispatch] = useStateValue();
   const [open, setOpen] = useState(false);
   const [selectedFile, setSelectedFile] = useState(null);
   const [loading, setLoading] = useState(false);
   const inputRef = useRef(null);
   const filePickerRef = useRef(null);
-
-  const handleModalOpen = () => {
-    setOpen(true);
-  };
-
+  const router = useRouter();
+  
+  // post upload function
   const handleUpload = async () => {
     if (loading) return;
     setLoading(true);
-    // create and add post to firestore
+    // create and add post details to firestore
     const docRef = await addDoc(collection(db, "posts"), {
       username: user?.displayName,
       caption: inputRef.current.value,
@@ -66,19 +76,33 @@ const UploadButton = () => {
     setOpen(false);
   };
 
+  // select a file for the image input
   const addImageToPost = (e) => {
     const reader = new FileReader();
     if (e.target.files[0]) {
+      // set the first image selected
       reader.readAsDataURL(e.target.files[0]);
     }
     reader.onload = (readerEvent) => {
       setSelectedFile(readerEvent.target.result);
     };
   };
+  
+  // logout user
+  const handleLogout = (e) => {
+    e.preventDefault();
+    signOut(auth).then(()=>{
+      dispatch({
+        type: "SET_USER",
+        user: null
+      })
+      router.push("/login")
+    }).catch(err =>  console.error(err))
+  }
 
   return (
     <div className="lg:hidden">
-      <Modal className="w-full" open={open} onClose={() => setOpen(false)}>
+       <Modal className="w-full" open={open} onClose={() => setOpen(false)}>
         <Box sx={style}>
           <div className="flex items-center px-2 py-2">
             <Avatar
@@ -99,7 +123,7 @@ const UploadButton = () => {
             />
             <button
               onClick={handleUpload}
-              disabled={!inputRef}
+              disabled={!selectedFile}
               className="flex bg-black text-white items-center border-none text-sm rounded px-2 py-1 font-bold"
             >
               {loading ? (
@@ -149,9 +173,20 @@ const UploadButton = () => {
         className="fixed right-0 bottom-1 z-50"
         sx={{ "& > :not(style)": { m: 1 } }}
       >
-        <Fab color="primary" onClick={handleModalOpen}>
-          <AddIcon />
-        </Fab>
+         <SpeedDial
+        ariaLabel="SpeedDial basic example"
+        sx={{ position: 'absolute', bottom: 16, right: 16 }}
+        icon={<FlakyIcon />}
+      >
+          <SpeedDialAction
+            icon={<AddIcon />}
+              onClick={()=>setOpen(true)}
+            tooltipTitle="Create post"
+            />
+                <SpeedDialAction icon={<ExitToAppIcon />} onClick={handleLogout} tooltipTitle="Logout" />
+
+
+      </SpeedDial>
       </Box>
     </div>
   );
