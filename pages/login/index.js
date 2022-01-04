e2b7ab9050602c836c37529224ecc39edc47dea4
Saad Siddiqui <saadsiddiqui927@gmail.com>
2022-01-04 19:00:25 +0530

    Add animation and code refactor

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,20 +1,21 @@
-import React, {useState} from "react";
+import { useState } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { auth, provider, db } from "../../firebase/firebase";
 import { useStateValue } from "../../context-api/StateProvider";
 import GoogleIcon from "@mui/icons-material/Google";
-import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
-import Link from "@mui/material/Link";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import {
+  Button,
+  TextField,
+  Link,
+  Box,
+  Typography,
+  Container,
+} from "@mui/material/";
 import { createUserWithEmailAndPassword, signInWithPopup } from "firebase/auth";
-import {collection, addDoc} from "firebase/firestore";
-import {useSpring, animated} from "react-spring"
+import { collection, addDoc } from "firebase/firestore";
+import { useSpring, animated } from "react-spring";
 import logo from "../../assets/connect-logo.png";
 
 function Copyright(props) {
@@ -35,16 +36,18 @@ function Copyright(props) {
   );
 }
 
-const theme = createTheme();
-
 export default function SignIn() {
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [{}, dispatch] = useStateValue();
   const router = useRouter();
   // animation config
-  const [state] = useState(true)
-  const { x } = useSpring({ from: { x: 2 }, x: state ? 1 : 0, config: { duration: 900 } })
+  const [state] = useState(true);
+  const { x } = useSpring({
+    from: { x: 2 },
+    x: state ? 1 : 0,
+    config: { duration: 900 },
+  });
 
   // to sign a new user
   const handleSignIn = (e) => {
@@ -56,15 +59,18 @@ export default function SignIn() {
           type: "SET_USER",
           user: result.user,
         });
+        // add user into the firestore 
         addDoc(collection(db, "users"), {
           username: result.user.displayName,
-          profilePic: result.user.photoURL
-        })
+          profilePic: result.user.photoURL,
+          email: result.user.email,
+        });
         router.push("/");
       })
       .catch((err) => console.log(err));
   };
 
+  // sign in with email and password
   const handleEmailSignIn = (e) => {
     e.preventDefault();
     createUserWithEmailAndPassword(auth, email, password)
@@ -81,140 +87,109 @@ export default function SignIn() {
     setPassword("");
   };
 
-  function Feature(){
-    const featureWords = [
-    "Share", "Read", "Shop", "Chat", "Explore", "Entertainment" 
-  ]
-
-    const animationDuration = 1.8
-  const animationLength = animationDuration * featureWords.length
-  const endOfAnimation = 100 / featureWords.length
-  const animationOverlap = 0.1
-
-    const slideWord = keyframes`
-  0% {
-    opacity: 0;
-    transform: translate3d(-50%, 25%, 0px);
-    visibility: visible;
-  }
-  ${1 - 1 * animationOverlap}% {
-    opacity: 1;
-    transform: translate3d(-50%, 75%, 0px);
-  }
-  ${endOfAnimation}% {
-    opacity: 1;
-    transform: translate3d(-50%, 75%, 0px);
-    visibility: visible;
-  }
-  ${endOfAnimation + endOfAnimation * animationOverlap}% {
-    opacity: 0;
-    transform: translate3d(-50%, 135%, 0px);
-    visibility: hidden;
-  }
-  100% {
-    opacity: 0;
-    visibility: visible;
-  }
-`
-
-  }
-
   return (
-        <animated.div
-        style={{
-          opacity: x.interpolate({ range: [0, 1], output: [0.3, 1] }),
-          transform: x
-            .interpolate({
-              range: [0, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 1],
-              output: [1, 0.97, 0.9, 1.1, 0.9, 1.1, 1.03, 1]
-            })
-            .interpolate(x => `scale(${x})`)
-        }}>
-
+    <animated.div
+      style={{
+        opacity: x.interpolate({ range: [0, 1], output: [0.3, 1] }),
+        transform: x
+          .interpolate({
+            range: [0, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 1],
+            output: [1, 0.97, 0.9, 1.1, 0.9, 1.1, 1.03, 1],
+          })
+          .interpolate((x) => `scale(${x})`),
+      }}
+    >
       <Head>
         <title>Login</title>
       </Head>
-        <div className="flex w-[100%] h-[100vh]  items-center">
-
-      <Container className="p-2 h-[100vh]  w-[100%] flex-col hidden md:inline" component="main" maxWidth="xs">
-        <Box className="grid h-[100%] gap-2 content-center">
-
-        <h1 className="font-bold text-8xl font-mono md:text-7xl">Connect</h1>
-          <p className="font-semibold text-gray-500 text-2xl ml-1 ">The best place to connect with people all over the world.</p>
-          </Box>
-
-      </Container>
-      <Container className="p-2" component="main" maxWidth="xs">
-        <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
+      <div className="flex w-[100%] h-[100vh]  items-center">
+        <Container
+          className="p-2 h-[100vh]  w-[100%] flex-col hidden md:inline"
+          component="main"
+          maxWidth="xs"
         >
-          <Image
-            src={logo}
-            alt=""
-            height={50}
-            width={50}
-            layout="fixed"
-            objectFit="contain"
-            className="cursor-pointer"
-          />
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box component="form" noValidate sx={{ mt: 1 }}>
-            <TextField
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              autoComplete="current-password"
-            />
-
-                <small className="w-full text-gray-400">*Signing In with email has been diabled temporarily. We will fix it soon.</small>
-            <Button
-              onClick={handleEmailSignIn}
-              type="submit"
-              fullWidth
-              sx={{ mt: 3, mb: 2 }}
-              className="bg-black text-white font-bold border-2  hover:bg-green-400"
-            >
-              Sign In
-            </Button>
-            <hr />
-            <p className="text-center my-2 text-gray-600 ">OR</p>
-            <Button
-              type="submit"
-              fullWidth
-              onClick={handleSignIn}
-              className="bg-black text-white font-bold hover:bg-blue-500"
-            >
-              Sign In With Google <GoogleIcon className="text-lg ml-2" />
-            </Button>
+          <Box className="grid h-[100%] gap-2 content-center">
+            <h1 className="font-bold text-8xl font-mono md:text-7xl">
+              Connect
+            </h1>
+            <p className="font-semibold text-gray-500 text-2xl ml-1 ">
+              The best place to connect with people all over the world.
+            </p>
           </Box>
-        </Box>
-        <Copyright sx={{ mt: 8, mb: 4 }} />
-      </Container>
-          </div>
+        </Container>
+        <Container className="p-2" component="main" maxWidth="xs">
+          <Box
+            sx={{
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Image
+              src={logo}
+              alt=""
+              height={50}
+              width={50}
+              layout="fixed"
+              objectFit="contain"
+              className="cursor-pointer"
+            />
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box component="form" noValidate sx={{ mt: 1 }}>
+              <TextField
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                autoComplete="current-password"
+              />
 
-          </animated.div>
+              <small className="w-full text-gray-400">
+                *Signing In with email has been diabled temporarily. We will fix
+                it soon.
+              </small>
+              <Button
+                onClick={handleEmailSignIn}
+                type="submit"
+                fullWidth
+                sx={{ mt: 3, mb: 2 }}
+                className="bg-black text-white font-bold border-2  hover:bg-green-400"
+              >
+                Sign In
+              </Button>
+              <hr />
+              <p className="text-center my-2 text-gray-600 ">OR</p>
+              <Button
+                type="submit"
+                fullWidth
+                onClick={handleSignIn}
+                className="bg-black text-white font-bold hover:bg-blue-500"
+              >
+                Sign In With Google <GoogleIcon className="text-lg ml-2" />
+              </Button>
+            </Box>
+          </Box>
+          <Copyright sx={{ mt: 8, mb: 4 }} />
+        </Container>
+      </div>
+    </animated.div>
   );
 }
