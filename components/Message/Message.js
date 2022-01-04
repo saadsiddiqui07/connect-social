e43bce6fd1288f5b82508cae41b0b2973f4698e5
Saad Siddiqui <saadsiddiqui927@gmail.com>
2022-01-04 18:50:16 +0530

    Integrate data from firestore

--- components/Message/Message.js
+++ components/Message/Message.js
@@ -1,8 +1,10 @@
 import { Avatar } from "@mui/material";
 import { useStateValue } from "../../context-api/StateProvider";
 
-const Message = ({ sender, message }) => {
+const Message = ({ message, messageSender, username, timestamp }) => {
   const [{ user }] = useStateValue();
+  // check if the message being sent is by the logged in user
+  const sender = user?.email === messageSender;
 
   return (
     <div className={`${sender ? "user" : "recipient"}`}>
@@ -12,10 +14,8 @@ const Message = ({ sender, message }) => {
         className="profile_picture"
       />
       <div className={`${sender ? "user__message" : "recipient__message"}`}>
-        <p className="text-sm font-bold">
-          Hey how are you and how's your work going on?
-        </p>
-        <small className={sender ? "text-red" : "text-green-50"}>8:45 pm</small>
+        <p className="text-sm font-bold">{message}</p>
+        <small>{new Date(timestamp?.toDate()).toLocaleDateString()}</small>
       </div>
     </div>
   );
