1006ea33fd8d0a84a32284518285507f5419e62d
Saad Siddiqui <saadsiddiqui927@gmail.com>
2022-01-06 20:26:41 +0530

    Add animation at the end of text message

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,9 +1,10 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useRouter } from "next/router";
 import { useStateValue } from "../context-api/StateProvider";
-import { Avatar, IconButton } from "@mui/material";
+import { Avatar, IconButton, MenuItem, Menu } from "@mui/material";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import SentimentSatisfiedOutlinedIcon from "@mui/icons-material/SentimentSatisfiedOutlined";
+import DeleteSweepOutlinedIcon from "@mui/icons-material/DeleteSweepOutlined";
 import SendOutlinedIcon from "@mui/icons-material/SendOutlined";
 import Message from "../components/Message/Message";
 import {
@@ -22,8 +23,18 @@ const ChatScreen = () => {
   const [input, setInput] = useState("");
   const [messages, setMessages] = useState([]);
   const [roomName, setRoomName] = useState("");
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const endOfMessageRef = useRef(null);
   const router = useRouter();
   const roomId = router.query.id;
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   // fetch room info with --> roomId
   useEffect(() => {
@@ -45,6 +56,13 @@ const ChatScreen = () => {
     }
   }, [roomId]);
 
+  // add scroll animation at the end of the message
+  const ScrollToBottom = () => {
+    endOfMessageRef.current.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  };
   // send message function
   const sendMessage = async (e) => {
     e.preventDefault();
@@ -56,6 +74,7 @@ const ChatScreen = () => {
       timestamp: serverTimestamp(),
     });
     setInput("");
+    ScrollToBottom();
   };
 
   return (
@@ -66,20 +85,37 @@ const ChatScreen = () => {
           <h4 className="font-bold text-sm">{roomName}</h4>
           <p className="text-xs">Last seen at 8:47 pm</p>
         </div>
-        <IconButton className="ml-auto">
+        <IconButton onClick={handleClick} className="ml-auto">
           <MoreVertIcon />
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              "aria-labelledby": "basic-button",
+            }}
+          >
+            <MenuItem onClick={handleClose}>
+              <DeleteSweepOutlinedIcon className="text-red-500 mr-2" /> Delete
+              Room
+            </MenuItem>
+          </Menu>
         </IconButton>
       </div>
       <div className="flex flex-1 p-3 flex-col overflow-y-scroll scrollbar-hide h-full">
         {messages.map((message) => (
-          <Message
-            key={message.id}
-            id={message.id}
-            message={message.data().message}
-            messageSender={message.data().email}
-            username={message.data().username}
-            timestamp={message.data().timestamp}
-          />
+          <>
+            <Message
+              key={message.data().id}
+              id={message.id}
+              message={message.data().message}
+              messageSender={message.data().email}
+              username={message.data().username}
+              timestamp={message.data().timestamp}
+            />
+            <div className="mb-[20px]" ref={endOfMessageRef} />
+          </>
         ))}
       </div>
       <form className="flex items-center border-t-2 border-black p-2">
