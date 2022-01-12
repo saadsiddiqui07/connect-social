fe8a00aee3422a788dd95f10587556ed10560469
Saad Siddiqui <saadsiddiqui927@gmail.com>
2022-01-13 00:29:55 +0530

    Add new anchor tag for redirect page

--- components/NewsCard/NewsCard.js
+++ components/NewsCard/NewsCard.js
@@ -1,8 +1,5 @@
-import * as React from "react";
 import {
-  Button,
   CardActionArea,
-  CardActions,
   Typography,
   CardContent,
   Card,
@@ -19,11 +16,12 @@ export default function NewsCard({
   if (!author) return null;
   return (
     <Card className="m-8 lg:m-10 shadow-lg">
+      <a  href={url}>
       <p className="py-1 px-2 font-semibold text-gray-700 font-mono">
         Source: {source.name}
       </p>
       <CardActionArea>
-        <img className="h-20 object-contain w-full" src={image} />
+        <img className="h-20 object-contain w-full" src={image} alt="NEWS" />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
             {title}
@@ -33,11 +31,7 @@ export default function NewsCard({
           </Typography>
         </CardContent>
       </CardActionArea>
-      <CardActions className="flex mt-auto">
-        <Button href={url} size="small" variant="contained" color="primary">
-          Show More
-        </Button>
-      </CardActions>
+        </a>
     </Card>
   );
 }
