972c8df407491aaf2f1658020048690097d922d5
Saad Siddiqui <saadsiddiqui927@gmail.com>
2022-01-13 00:33:01 +0530

    Add drawer component and refactor code

--- pages/market/index.js
+++ pages/market/index.js
@@ -3,31 +3,16 @@ import Head from "next/head";
 import Product from "../../components/Product/Product";
 import Header from "../../components/Header/Header";
 import MobileHeader from "../../components/MobileHeader/MobileHeader";
-// import { useStateValue } from "../../context-api/StateProvider";
-// import Badge from "@mui/material/Badge";
-// import { styled } from "@mui/material/styles";
-// import IconButton from "@mui/material/IconButton";
-// import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-// import CheckoutProduct from "../../components/CheckoutProducts/CheckoutProducts";
-// import Subtotal from "../../components/Subtotal/Subtotal";
-
-// const StyledBadge = styled(Badge)(({ theme }) => ({
-//   "& .MuiBadge-badge": {
-//     right: -3,
-//     top: 13,
-//     border: `2px solid ${theme.palette.background.paper}`,
-//     padding: "0 4px",
-//   },
-// }));
+import { useStateValue } from "../../context-api/StateProvider";
+import Badge from "@mui/material/Badge";
+import IconButton from "@mui/material/IconButton";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { Drawer } from "@mui/material";
+import Cart from "../../components/Cart/Cart";
 
 const MarketPlace = ({ products }) => {
-  const [showCartItems, setShowCartItems] = useState(false);
-  // const [{ cart }] = useStateValue();
-
-  // const showItems = (e) => {
-  //   e.preventDefault();
-  //   setShowCartItems(!showCartItems);
-  // };
+  const [open, setOpen] = useState(false);
+  const [{ cart }] = useStateValue();
 
   return (
     <div>
@@ -35,9 +20,20 @@ const MarketPlace = ({ products }) => {
         <title>Connect: MarketPlace</title>
         <meta name="MarketPlace" content="Socialize in a better way" />
       </Head>
+      <Drawer anchor="right" open={open} onClose={() => setOpen(false)}>
+        <Cart setOpen={setOpen} />
+      </Drawer>
+
       <Header />
       <div className="sm:hidden">
         <MobileHeader />
+      </div>
+      <div className="flex items-center ">
+        <IconButton onClick={() => setOpen(true)} className="ml-auto pr-4">
+          <Badge badgeContent={cart?.length} color="success">
+            <ShoppingCartIcon className="text-blue-500" fontSize="large" />
+          </Badge>
+        </IconButton>
       </div>
       <div className="p-5 flex flex-col-reverse md:flex-row">
         <div className="px-5 bg-gray-50 overflow-y-scroll scrollbar-hide center-center sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
@@ -45,42 +41,11 @@ const MarketPlace = ({ products }) => {
             <Product
               key={product.id}
               id={product.id}
-              category={product.category}
-              description={product.description}
-              image={product.image}
-              price={product.price}
-              title={product.title}
-              rating={product.rating}
+              {...product}
+              setOpen={setOpen}
             />
           ))}
         </div>
-        {/* <div className="w-[80%] h-[550px] overflow-y-scroll scrollbar-hidden flex-col">
-          <div className="flex items-center px-3 border-b-2 border-gray-300">
-            <h3 className="font-extrabold font-mono text-md md:text-lg">
-              Your cart
-            </h3>
-            <IconButton className="flex ml-auto pr-5 md:ml-auto">
-              <StyledBadge badgeContent={`${cart?.length}`} color="primary">
-                <ShoppingCartIcon className="text-black text-3xl" />
-              </StyledBadge>
-            </IconButton>
-          </div>
-          <div className="hidden lg:inline-flex justify-center items-center flex-col">
-            {cart?.length > 0 && <Subtotal />}
-            {cart?.map((product) => (
-              <CheckoutProduct
-                key={product.id}
-                id={product.id}
-                category={product.category}
-                description={product.description}
-                image={product.image}
-                price={product.price}
-                title={product.title}
-                rating={product.rating}
-              />
-            ))}
-          </div>
-        </div> */}
       </div>
     </div>
   );
