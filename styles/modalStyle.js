f78556b077e1e696ccdb77cad944b143f082114c
Saad Siddiqui <saadsiddiqui927@gmail.com>
2022-01-19 16:31:40 +0530

    Add global style for modal

---
+++ styles/modalStyle.js
@@ -0,0 +1,12 @@
+export const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "white",
+  border: "2px solid lightgray",
+  boxShadow: 24,
+  p: 2,
+  borderRadius: 2,
+};
