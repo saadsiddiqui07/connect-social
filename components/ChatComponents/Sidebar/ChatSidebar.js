0583bdf1bd10474e1732e8d25a2fdd1e8590b649
Saad Siddiqui <saadsiddiqui927@gmail.com>
2022-01-20 18:10:35 +0530

    Import modal style globally

--- components/ChatComponents/Sidebar/ChatSidebar.js
+++ components/ChatComponents/Sidebar/ChatSidebar.js
@@ -12,19 +12,7 @@ import {
 } from "firebase/firestore";
 import { db } from "../../../firebase/firebase";
 import Chat from "../Chat/Chat";
-
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "white",
-  border: "2px solid lightgray",
-  boxShadow: 24,
-  p: 2,
-  borderRadius: 2,
-};
+import { style } from "../../../styles/modalStyle";
 
 const ChatSidebar = () => {
   const [{ user }] = useStateValue();
