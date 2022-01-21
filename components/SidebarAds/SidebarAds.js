5d2507e243f224189dd5817a2b34fdc490f05916
Saad Siddiqui <saadsiddiqui927@gmail.com>
2022-01-21 15:58:28 +0530

    Add button to post an ad

--- components/SidebarAds/SidebarAds.js
+++ components/SidebarAds/SidebarAds.js
@@ -1,14 +1,26 @@
 import Image from "next/image";
+import { Button } from "@mui/material";
+import CampaignIcon from "@mui/icons-material/Campaign";
 
 const SidebarAds = () => {
   return (
     <div className="hidden flex-1 lg:inline cursor-pointer bg-white p-2 rounded shadow-lg ml-5 mr-6 mb-auto">
-      <Image
-        src="https://ppcprotect.com/static/2462efe5633121a7b298d08f21e8f5da/ga-worth-it-thumbnail.jpg"
-        height={500}
-        width={400}
-        objectFit="contain"
-      />
+      <div>
+        <Image
+          src="https://ppcprotect.com/static/2462efe5633121a7b298d08f21e8f5da/ga-worth-it-thumbnail.jpg"
+          height={500}
+          width={400}
+          objectFit="contain"
+        />
+      </div>
+      <Button
+        className="text-white ml-auto bg-gradient-to-r from-blue-500 to-blue-500 hover:from-green-400 hover:to-blue-500"
+        color="inherit"
+        variant="contained"
+        endIcon={<CampaignIcon />}
+      >
+        Post an ad
+      </Button>
     </div>
   );
 };
