aa7c98e879d12a5440776f13564d99a5256fdc67
Saad Siddiqui <saadsiddiqui927@gmail.com>
2022-01-22 00:54:50 +0530

    Code refactor

--- components/Post/Post.js
+++ components/Post/Post.js
@@ -77,87 +77,90 @@ const Post = ({
   const showComments = showCommentInput && comments.length > 0;
 
   return (
-    <Card className="m-5 w-4/5 ml-auto mr-auto">
-      <CardHeader
-        avatar={<Avatar src={profileImg} />}
-        action={
-          <IconButton onClick={deletePost} aria-label="settings">
-            {showDeleteIcon && <DeleteForeverIcon />}
-          </IconButton>
-        }
-        title={username}
-        subheader={new Date(timestamp?.toDate()).toLocaleString(undefined, {
-          timeZone: "Asia/Kolkata",
-        })}
-      />
-      {image && (
-        <CardMedia
-          className="object-contain h-60"
-          component="img"
-          height="194"
-          image={image}
-          alt="Paella dish"
+    <>
+      <Card className="m-5 w-4/5 ml-auto mr-auto">
+        <CardHeader
+          avatar={<Avatar src={profileImg} />}
+          action={
+            <IconButton onClick={deletePost} aria-label="settings">
+              {showDeleteIcon && <DeleteForeverIcon />}
+            </IconButton>
+          }
+          title={username}
+          subheader={new Date(timestamp?.toDate()).toLocaleString(undefined, {
+            timeZone: "Asia/Kolkata",
+          })}
         />
-      )}
-      <CardContent>
-        <Typography
-          className="flex text-black"
-          variant="body2"
-          color="text.secondary"
-        >
-          <Typography className="font-bold text-black mr-2">
-            {username.split(" ").join("").toLowerCase()}
+        {image && (
+          <CardMedia
+            className="object-contain h-60"
+            component="img"
+            height="194"
+            image={image}
+            alt="Paella dish"
+          />
+        )}
+        <CardContent>
+          <Typography
+            className="flex text-black"
+            component="p"
+            variant="body2"
+            color="text.secondary"
+          >
+            <Typography className="font-bold text-black mr-2">
+              {username.split(" ").join("").toLowerCase()}
+            </Typography>
+            {caption}
           </Typography>
-          {caption}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <IconButton onClick={() => setIsLiked(!isLiked)}>
-          {!isLiked ? (
-            <FavoriteBorderIcon />
-          ) : (
-            <FavoriteIcon className="text-blue-600" />
-          )}
-        </IconButton>
-        <IconButton onClick={() => setShowCommentInput(!showCommentInput)}>
-          <ChatBubbleOutlineIcon />{" "}
-          <p className="text-sm ml-2">{comments.length} comments</p>
-        </IconButton>
-      </CardActions>
-      {/* Comments section */}
-      {!showComments && (
-        <>
-          <div>
-            {comments.map((comment, idx) => (
-              <div key={idx} className="flex items-center px-2">
-                <p className="text-xs font-extrabold">
-                  {comment.data().username}
-                </p>
-                <span className="ml-3 text-xs md:text-sm">
-                  {comment.data().comment}
-                </span>
-              </div>
-            ))}
-          </div>
-          <form className="flex px-1 py-2">
-            <input
-              value={comment}
-              onChange={(e) => setComment(e.target.value)}
-              placeholder="Leave a comment"
-              className="border-none h-10 pl-3 bg-gray-100 flex-1 focus:ring-0 outline-none"
-            />
-            <button
-              onClick={commentOnPost}
-              type="submit"
-              disabled={!comment.trim()}
-              className="bg-blue-500 font-mono text-white px-2 ml-3 rounded"
-            >
-              Post
-            </button>
-          </form>
-        </>
-      )}
-    </Card>
+        </CardContent>
+        <CardActions>
+          <IconButton onClick={() => setIsLiked(!isLiked)}>
+            {!isLiked ? (
+              <FavoriteBorderIcon />
+            ) : (
+              <FavoriteIcon className="text-blue-600" />
+            )}
+          </IconButton>
+          <IconButton onClick={() => setShowCommentInput(!showCommentInput)}>
+            <ChatBubbleOutlineIcon />{" "}
+            <p className="text-sm ml-2">{comments.length} comments</p>
+          </IconButton>
+        </CardActions>
+        {/* Comments section */}
+        {!showComments && (
+          <>
+            <div>
+              {comments.map((comment, idx) => (
+                <div key={idx} className="flex items-center px-2">
+                  <p className="text-xs font-extrabold">
+                    {comment.data().username}
+                  </p>
+                  <span className="ml-3 text-xs md:text-sm">
+                    {comment.data().comment}
+                  </span>
+                </div>
+              ))}
+            </div>
+            <form className="flex px-1 py-2">
+              <input
+                value={comment}
+                onChange={(e) => setComment(e.target.value)}
+                placeholder="Leave a comment"
+                className="border-none h-10 pl-3 bg-gray-100 flex-1 focus:ring-0 outline-none"
+              />
+              <button
+                onClick={commentOnPost}
+                type="submit"
+                disabled={!comment.trim()}
+                className="bg-blue-500 font-mono text-white px-2 ml-3 rounded"
+              >
+                Post
+              </button>
+            </form>
+          </>
+        )}
+      </Card>
+    </>
   );
 };
 
