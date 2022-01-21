f2a711916fb6129448b9e30d2eb24f2b3ef1c7fa
Saad Siddiqui <saadsiddiqui927@gmail.com>
2022-01-22 00:58:01 +0530

    Add new props of API

--- components/SidebarOptions/SidebarOptions.js
+++ components/SidebarOptions/SidebarOptions.js
@@ -1,21 +1,15 @@
 import { Avatar } from "@mui/material";
-import { useStateValue } from "../../context-api/StateProvider";
 
-const SidebarOptions = ({ username, profilePic, email }) => {
-  const [{ user }] = useStateValue();
-
-  // if logged in as a user then do not show username in sidebar
-  if (email === user?.email) return null;
-
+const SidebarOptions = ({ avatar, firstName, lastName }) => {
   return (
     <div
       className="flex mt-5 pl-5 pr-10  items-center 
     cursor-pointer transition duration-100 transform hover:bg-gray-200 hover:scale-110 "
     >
-      <Avatar src={profilePic} />
+      <Avatar src={avatar} />
       <div className="hidden lg:inline-flex">
         <p className="text-xs font-bold text-gray-500 ml-3">
-          {username.split(" ").join("").toLowerCase()}
+          {firstName} {lastName}
         </p>
       </div>
     </div>
