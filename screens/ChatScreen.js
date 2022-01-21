8c7bf32853d859ed61586162e5d79eb111c738db
Saad Siddiqui <saadsiddiqui927@gmail.com>
2022-01-21 16:00:16 +0530

    Add modal to update room name

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,11 +1,10 @@
 import { useState, useEffect, useRef } from "react";
 import { useRouter } from "next/router";
 import { useStateValue } from "../context-api/StateProvider";
-import { Avatar, IconButton, MenuItem, Menu } from "@mui/material";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { Avatar, IconButton, Button, Modal, Box } from "@mui/material";
 import SentimentSatisfiedOutlinedIcon from "@mui/icons-material/SentimentSatisfiedOutlined";
-import DeleteSweepOutlinedIcon from "@mui/icons-material/DeleteSweepOutlined";
 import SendOutlinedIcon from "@mui/icons-material/SendOutlined";
+import ModeEditOutlineOutlinedIcon from "@mui/icons-material/ModeEditOutlineOutlined";
 import Message from "../components/Message/Message";
 import {
   addDoc,
@@ -15,25 +14,34 @@ import {
   query,
   doc,
   orderBy,
+  updateDoc,
 } from "firebase/firestore";
 import { db } from "../firebase/firebase";
+import { style } from "../styles/modalStyle";
 
 const ChatScreen = () => {
   const [{ user }] = useStateValue();
   const [input, setInput] = useState("");
   const [messages, setMessages] = useState([]);
   const [roomName, setRoomName] = useState("");
-  const [anchorEl, setAnchorEl] = useState(null);
-  const open = Boolean(anchorEl);
+  const [newRoomName, setNewRoomName] = useState("");
+  const [roomAdmin, setRoomAdmin] = useState("");
+  const [openModal, setOpenModal] = useState(false);
   const endOfMessageRef = useRef(null);
   const router = useRouter();
   const roomId = router.query.id;
 
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
+  // edit new room name
+  const handleUpdateRoomName = async () => {
+    await updateDoc(doc(db, "chatRooms", roomId), {
+      name: newRoomName,
+    });
+    setOpenModal(false);
+    setNewRoomName("");
   };
-  const handleClose = () => {
-    setAnchorEl(null);
+
+  const openEditModal = () => {
+    setOpenModal(true);
   };
 
   // fetch room info with --> roomId
@@ -42,6 +50,7 @@ const ChatScreen = () => {
       onSnapshot(doc(db, "chatRooms", roomId), (snapshot) => {
         // set room name
         setRoomName(snapshot.data().name);
+        setRoomAdmin(snapshot.data().createdBy);
         // fetch messages of a specific chatRoom using roomId
       });
       onSnapshot(
@@ -63,6 +72,7 @@ const ChatScreen = () => {
       block: "start",
     });
   };
+
   // send message function
   const sendMessage = async (e) => {
     e.preventDefault();
@@ -79,28 +89,64 @@ const ChatScreen = () => {
 
   return (
     <div className="h-full flex flex-col ">
+      <Modal
+        className="w-full"
+        open={openModal}
+        onClose={() => setOpenModal(false)}
+      >
+        <Box sx={style}>
+          <h4 className="text-center font-sans">Update new room name</h4>
+          {user?.email === roomAdmin ? (
+            <>
+              <div className="flex items-center px-2 py-2">
+                <Avatar
+                  className="h-10 object-contain rounded-full mr-2"
+                  src={user?.photoURL}
+                  alt="img"
+                />
+                <input
+                  value={newRoomName}
+                  onChange={(e) => setNewRoomName(e.target.value)}
+                  className="bg-transparent flex-1 ml-2 text-black text-sm border-1 border-b-2 outline-none"
+                  placeholder="Enter email or username..."
+                />
+              </div>
+              <Button
+                disabled={!newRoomName}
+                onClick={handleUpdateRoomName}
+                color="primary"
+                size="small"
+                variant="contained"
+                className="flex ml-auto mr-auto"
+              >
+                Edit
+              </Button>
+            </>
+          ) : (
+            <div className="flex flex-col justify-center items-center p-2">
+              <h4 className="text-gray-700 text-sm font-mono mb-2">
+                You cannot update the room name as you are not the room admin.
+              </h4>
+              <Button
+                color="error"
+                size="small"
+                variant="contained"
+                onClick={() => setOpenModal(false)}
+              >
+                Close
+              </Button>
+            </div>
+          )}
+        </Box>
+      </Modal>
       <div className="flex items-center px-3 py-2 border-b-2 border-black ">
         <Avatar />
         <div className="ml-5">
           <h4 className="font-bold text-sm">{roomName}</h4>
           <p className="text-xs">Last seen at 8:47 pm</p>
         </div>
-        <IconButton onClick={handleClick} className="ml-auto">
-          <MoreVertIcon />
-          <Menu
-            id="basic-menu"
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            MenuListProps={{
-              "aria-labelledby": "basic-button",
-            }}
-          >
-            <MenuItem onClick={handleClose}>
-              <DeleteSweepOutlinedIcon className="text-red-500 mr-2" /> Delete
-              Room
-            </MenuItem>
-          </Menu>
+        <IconButton onClick={openEditModal} className="ml-auto">
+          <ModeEditOutlineOutlinedIcon className="text-blue-500" />
         </IconButton>
       </div>
       <div className="flex flex-1 p-3 flex-col overflow-y-scroll scrollbar-hide h-full">
