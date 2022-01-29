02c275b23634b53ad2848469e526409ac103c214
Saad Siddiqui <saadsiddiqui927@gmail.com>
2022-01-30 01:41:19 +0530

    Add profile image in message

--- components/Message/Message.js
+++ components/Message/Message.js
@@ -1,7 +1,7 @@
 import { Avatar } from "@mui/material";
 import { useStateValue } from "../../context-api/StateProvider";
 
-const Message = ({ message, messageSender, username, timestamp }) => {
+const Message = ({ message, messageSender, profileImg, timestamp }) => {
   const [{ user }] = useStateValue();
   // check if the message being sent is by the logged in user
   const sender = user?.email === messageSender;
@@ -9,7 +9,7 @@ const Message = ({ message, messageSender, username, timestamp }) => {
   return (
     <div className={`${sender ? "user" : "recipient"}`}>
       <Avatar
-        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+        src={profileImg}
         alt={user?.displayName[0]}
         className="profile_picture"
       />
