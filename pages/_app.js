8a08aa0304360e5173402df00677033592f4a027
Saad Siddiqui <saadsiddiqui927@gmail.com>
2022-02-02 17:19:55 +0530

    Add client side style config for entire application

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,33 @@
 import "../styles/globals.css";
-import { useEffect } from "react";
+import Head from "next/head";
 import reducer, { initialState } from "../context-api/reducer";
 import { StateProvider } from "../context-api/StateProvider";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { CacheProvider } from "@emotion/react";
+import createEmotionCache from "../src/createEmotionCache";
+import theme from "../src/theme";
 
-function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    // remove the server-side injected CSS
-    const jsStyles = document.querySelector("#js-server-side");
-    if (jsStyles) {
-      jsStyles.parentElement.removeChild(jsStyles);
-    }
-  }, []);
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
 
+function MyApp({
+  Component,
+  pageProps,
+  emotionCache = clientSideEmotionCache,
+}) {
   return (
-    <StateProvider initialState={initialState} reducer={reducer}>
-      <Component {...pageProps} />
-    </StateProvider>
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <StateProvider initialState={initialState} reducer={reducer}>
+          <Component {...pageProps} />
+        </StateProvider>
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
 
