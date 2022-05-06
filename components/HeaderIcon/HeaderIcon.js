8a1e6b91d979d62d8e21cd7013b6a937358a6848
Saad Siddiqui <saadsiddiqui927@gmail.com>
2022-05-06 16:45:14 +0530

    Add active tab stylings

--- components/HeaderIcon/HeaderIcon.js
+++ components/HeaderIcon/HeaderIcon.js
@@ -1,12 +1,19 @@
+import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import { Tooltip } from "@mui/material";
 
 const HeaderIcon = ({ Icon, title }) => {
+  const [active, setActive] = useState(false);
   const router = useRouter();
+  const tabTitle = router.pathname;
 
   const handleClick = () => {
     router.push(`/${title}`);
   };
+
+  useEffect(() => {
+    tabTitle === `/${title}` ? setActive(true) : setActive(false);
+  }, [tabTitle, title]);
 
   return (
     <Tooltip className="capitalize" title={!title ? "home" : title}>
@@ -14,9 +21,11 @@ const HeaderIcon = ({ Icon, title }) => {
         onClick={handleClick}
         className={`flex items-center 
     cursor-pointer 
-    sm:h-10 px-5 md:h-15 px-7 hover:bg-gray-100 lg:px-12 h-22 2xl:px-10 h-22
+    sm:h-10 px-5 md:h-15 px-7 hover:bg-gray-100 lg:px-12 h-22 max-w-fit 2xl:px-10 h-22
     active:border-b-2
-    active:border-gray-500 group`}
+    active:border-gray-500 group ${
+      active && "md:border-b-2 md:border-solid md:border-black md:rounded-sm"
+    }`}
       >
         <Icon className="hidden sm:inline-flex text-12 md:inline-flex text-2xl lg:inline-flex lg:text-3xl" />
         <p className="hidden">{title}</p>
@@ -24,4 +33,5 @@ const HeaderIcon = ({ Icon, title }) => {
     </Tooltip>
   );
 };
+
 export default HeaderIcon;
